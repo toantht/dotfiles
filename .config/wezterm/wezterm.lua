a489ead7310c6394114f8627d0a34f9526a34035
toantht <trantoan2991@gmail.com>
2025-01-18 22:45:34 +0700

    added in wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+config.font_size = 16
+
+-- For example, changing the color scheme:
+config.color_scheme = "Tokyo Night"
+
+-- config.enable_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.9
+
+-- and finally, return the configuration to wezterm
+return config
